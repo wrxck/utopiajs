0ea999a3dd4ea657921cb21c5cfdd2898cbc00f6
Matt Hesketh <matt@heskethwebdesign.co.uk>
2026-02-08 21:17:04 +0000

    Add ESLint flat config for CI
    Add eslint.config.js (ESLint 9 flat config format) with TypeScript
    support and Node/browser globals so the lint CI job passes.

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['packages/*/src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/template/**'],
+  },
+]
